e481390e6af3a0577110bfa1a64349a84f82fda3
lplanque <lplanque@gmail.com>
2014-10-20 08:51:38 +0200

    Optimization of Templates class

--- jongopp/src/main/java/com/lplanque/jongo/pp/lang/Templates.java
+++ jongopp/src/main/java/com/lplanque/jongo/pp/lang/Templates.java
@@ -4,19 +4,24 @@ import static java.lang.String.format;
 
 public final class Templates {
 	
-	public static final Template EMPTY = new Template() {
-		
-		@Override public String toString() {
-			return "";
-		}
-		
-		@Override public int arity() {
-			return 0;
-		}
-	};
+	public static final Template EMPTY = template("", 0);
 	
 	// BUILDER METHODS
 	// ---------------
+	
+	private static Template template(final String pattern, final int arity) {
+		
+		return new Template() {
+			
+			@Override public String toString() {
+				return pattern;
+			}
+			
+			@Override public int arity() {
+				return arity;
+			}
+		};
+	}
 	
 	public static Template empty() {
 		return EMPTY;
@@ -27,102 +32,50 @@ public final class Templates {
 	}
 	
 	public static Template array(Template template) {
-		
 		final Template effective = emptyIfNull(template);
-		return new Template() {
-			
-			@Override public String toString() {
-				return format("[%s]", effective);
-			}
-			
-			@Override public int arity() {
-				return effective.arity();
-			}
-		};
+		return template(format("[%s]", effective), effective.arity());
 	}
 	
 	public static Template brace(final Template template) {
-		
 		final Template effective = emptyIfNull(template);
-		return new Template() {
-			
-			@Override public String toString() {
-				return format("{%s}", effective);
-			}
-			
-			@Override public int arity() {
-				return effective.arity();
-			}
-		};
+		return template(format("{%s}", effective), effective.arity());
 	}
 	
 	public static Template sharps(final int n) {
-		
-		return n <= 0? EMPTY: new Template() {
-			
-			@Override public String toString() {
-				final StringBuilder builder = new StringBuilder();
-				builder.append('#');
-				for(int i = 1; i < n; i++) {
-					builder.append(',').append('#');
-				}
-				return builder.toString();
+		if(n <= 0) {
+			return EMPTY;
+		} else {
+			final StringBuilder builder = new StringBuilder();
+			builder.append('#');
+			for(int i = 1; i < n; i++) {
+				builder.append(',').append('#');
 			}
-			
-			@Override public int arity() {
-				return n;
-			}
-		};
+			return template(builder.toString(), n);
+		}
 	}
 	
 	public static Template seq(final Template first, final Template... rem) {
-		
-		return first == null || rem == null? EMPTY: new Template() {
-			
-			@Override public String toString() {
-				final StringBuilder builder = new StringBuilder();
-				builder.append(first); 
-				if(rem != null) {
-					for(Object o: rem) {
-						builder.append(',').append(o);
-					}
+		if(first == null || rem == null) {
+			return EMPTY;
+		} else {
+			final StringBuilder builder = new StringBuilder();
+			builder.append(first); 
+			if(rem != null) {
+				for(Object o: rem) {
+					builder.append(',').append(o);
 				}
-				return builder.toString();				
 			}
-			
-			@Override public int arity() {
-				return arities(first, rem);
-			}
-		};
+			return template(builder.toString(), arities(first, rem));
+		}
 	}
 	
 	public static Template tuple(final Object left) {
-
-		return new Template() {
-			
-			@Override public String toString() {
-				return format("%s:#", left);
-			}
-			
-			@Override public int arity() {
-				return 1;
-			}
-		};
+		return template(format("%s:#", left), 1);
 	}
 	
 	public static Template tuple(final Object left, final Template right) {
-		
 		final Template effective = emptyIfNull(right);
-		return new Template() {
-			
-			@Override public String toString() {
-				return format("%s:%s", left, effective);
-			}
-			
-			@Override public int arity() {
-				return effective.arity();
-			}
-		};
+		return template(format("%s:%s", left, effective), effective.arity());
 	}
 	
 	// ARITY BUILDER
